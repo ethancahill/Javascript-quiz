e20103e5f4e9900cbbc0fc2598b9529ffc907f65
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-07-29 08:53:25 -0400

    added time

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var startBtn = document.getElementById('startbtn')
-
+var time = 90
 function start() {
-    
+    setInterval(function(){
+        time = time - 1
+    })
 }
 
 startBtn.addEventListener('click', start)
