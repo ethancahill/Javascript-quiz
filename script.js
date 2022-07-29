f1a510efe994bc07ba90895e00442ab357593371
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-07-29 09:49:04 -0400

    added click listener

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var answer1 =document.getElementById('question1')
 var answer2 =document.getElementById('question2')
 var answer3 =document.getElementById('question3')
 var answer4 =document.getElementById('question4')
+var answerCorrectness = document.getElementById('right-wrong')
 
 var questionAnswers = [
     answer1,
@@ -17,26 +18,26 @@ var questions = [
   {
     question: "Which of these is a coding language?",
     answers: ["java", "coffee", "cafe", "tea"],
-    answerIndex: 0,
+    correctAnswer: 0,
   },
   {
     question: "Which of these is an HTML element?",
     answers: ["<div>", "<dark>", "<jedi>", "<mindtrick>"],
-    answerIndex: 0,
+    correctAnswer: 0,
   },
   {
     question: "What can be used to insert data into an array?",
     answers: ["kick", "shove", "push", "force"],
-    answerIndex: 2,
+    correctAnswer: 2,
   },
   {
     question: "Who created react?",
     answers: ["google", "facebook", "jeff bezos", "apple"],
-    answerIndex: 1,
+    correctAnswer: 1,
   },
 ];
 
-let questionNumber = [0]
+let questionNumber = 0
 
 function loadQuestion(question) {
     questionDisplay.textContent = question.question
@@ -46,7 +47,27 @@ function loadQuestion(question) {
     }
 }
 
+function clickHandler(e) {
+    var clicked = e.target;
+    var answer = clicked.getAttribute('data-answer')
+    if(answer == questions[questionNumber].correctAnswer) {
+        answerCorrectness.textContent = 'Correct'
+        answeredCorrect()
+    } else {
+        answerCorrectness.textContent = 'Wrong'
+        answeredWrong()
+    }
+    questionNumber++;
+    loadQuestion(questions[questionNumber])
+}
 
+function answeredCorrect {
+
+}
+
+function answeredWrong {
+
+}
 
 function start() {
   var timer = setInterval(function () {
@@ -60,4 +81,11 @@ function start() {
   loadQuestion(questions[questionNumber])
 }
 
+function clickListeners() {
+    for (var i = 0; i < questionAnswers.length; i++) {
+        questionAnswers[i].addEventListener('click', clickHandler)
+    }
+}
+
+clickListeners()
 startBtn.addEventListener("click", start);
