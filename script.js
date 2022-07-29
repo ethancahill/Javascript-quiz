ebd672590ff4c9662c386385147332f25ce9978b
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-07-29 09:07:07 -0400

    reset

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-var startBtn = document.getElementById('startbtn')
-var time = 90
-function start() {
-  var timer =  setInterval(function(){
-        time = time - 1
-        if(time <= 0){
-            clearInterval()
-        } else {
+var startBtn = document.getElementById("startbtn");
+var time = 90;
+var questionDisplay = document.getElementById('quiz-header') 
+var questions = [
+  {
+    question: "Which of these is a coding language",
+    answers: ["java", "coffee", "cafe", "tea"],
+    answerIndex: 0,
+  },
+  {
+    question: "Which of these is an HTML element?",
+    answers: ["<div>", "<dark>", "<jedi>", "<mindtrick>"],
+    answerIndex: 0,
+  },
+  {
+    question: "What can be used to insert data into an array",
+    answers: ["kick", "shove", "push", "force"],
+    answerIndex: 2,
+  },
+  {
+    question: "Who created react?",
+    answers: ["google", "facebook", "jeff bezos", "apple"],
+    answerIndex: 1,
+  },
+];
 
-        }
-    })
+function start() {
+  var timer = setInterval(function () {
+    time = time - 1;
+    if (time <= 0) {
+      clearInterval(timer);
+    } else {
+    }
+  });
 }
 
-startBtn.addEventListener('click', start)
+startBtn.addEventListener("click", start);
